6d7ff8a84202e8460972972a1015059598d730e4
Fernndo Viton <fcode@viton.org>
2017-01-28 21:48:07 -0800

    Move gameTImer out of middleware

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import Root from './components/root';
 import root from './reducers/root';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { startGameTimer } from './middleware/gameTimer'
+import { startGameTimer } from './util/gameTimer'
 import reduxLogger from './middleware/reduxLogger'
 
 const store = createStore(root, applyMiddleware(reduxLogger));
